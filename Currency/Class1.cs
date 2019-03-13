6e172999bda30a188cf58a98f1ecbb4f821c3b03
Bret Hayes <bret_hayes@comcast.net>
2019-03-13 10:10:55 -0400

    math for currency

--- Currency/Class1.cs
+++ Currency/Class1.cs
@@ -11,22 +11,22 @@ namespace Currency
         int crate = 100; 
         public void MetalCreds() //metal gets a base modifier of 2
         {
-            int metal = crate * 2
+            int metal = crate * 2;
         }
 
         public void FabricCreds() // fabrics gets a base modifier of 1.5
         {
-
+            double fabric = crate * 1.5;
         }
 
         public void GemstoneCreds() // gemstones get a base modifier of 6
         {
-
+            int gemstone = crate * 6;
         }
 
         public void Supplies() // supplies gets a base modifier of 1
         {
-            
+            int supplies = crate * 1;
         }
     }
 }

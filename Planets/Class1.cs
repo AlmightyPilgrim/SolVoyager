67f4c826f3e1d06c7b6a263510e768c9acb7914c
Bret Hayes <bret_hayes@comcast.net>
2019-03-14 07:17:30 -0400

    Planet Resource methods

--- Planets/Class1.cs
+++ Planets/Class1.cs
@@ -10,21 +10,93 @@ namespace Planets
 {
     public class Earth
     {
+        public int EarthMetals()
+        {
+            
+        }
 
+        public double EarthFabric()
+        {
+
+        }
+
+        public int EarthGemstone()
+        {
+
+        }
+
+        public int EarthSupply()
+        {
+
+        }
     }
 
     public class Mars
     {
+        public int MarsMetals()
+        {
 
+        }
+
+        public double MarsFabric()
+        {
+
+        }
+
+        public int MarsGemstone()
+        {
+
+        }
+
+        public int MarsSupply()
+        {
+
+        }
     }   
     
     public class MiningColony
     {
+        public int ColonyMetals()
+        {
 
+        }
+
+        public double ColonyFabric()
+        {
+
+        }
+
+        public int ColonyGemstone()
+        {
+
+        }
+
+        public int ColonySupply()
+        {
+
+        }
     }
 
     public class AlphaCentari
     {
+        public int CentariMetals()
+        {
 
+        }
+
+        public double CentariFabric()
+        {
+
+        }
+
+        public int CentariGemstone()
+        {
+
+        }
+
+        public int CentariSupply()
+        {
+
+        }
     }
 }

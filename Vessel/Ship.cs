a9afcfe7ec59dbc8c961d2a52b80f2c7a3d4e070
Bret Hayes <bret_hayes@comcast.net>
2019-03-19 10:37:30 -0400

    Moved warp into vessel, removed warp's class library

--- Vessel/Ship.cs
+++ Vessel/Ship.cs
@@ -21,6 +21,16 @@ namespace Vessel
             return x;
         }
 
+        public double WarpEquation(int warp)
+        {
+            double x = 10 / 3.0;
+            double y = -11 / 3.0;
+            warp = 5;
+            double z = 10 - warp;
+            double velocity = (Math.Pow(warp, x)) + (Math.Pow(z, y));
+            Console.WriteLine($"Warp spped is {velocity}");
+            return velocity;
+        }
 
     }
 }
--- Warp/Class1.cs
+++ Warp/Class1.cs
@@ -8,16 +8,7 @@ namespace Warp
 {
     public class WarpSpeed
     {
-        public double WarpEquation(int warp)
-        {
-            double x = 10 / 3.0;
-            double y = -11 / 3.0;
-            warp = 5;
-            double z = 10 - warp;
-            double velocity = (Math.Pow(warp, x)) + (Math.Pow(z, y));
-            Console.WriteLine($"Warp spped is {velocity}");
-            return velocity;
-        }
+        
     }
 
     public class Travel

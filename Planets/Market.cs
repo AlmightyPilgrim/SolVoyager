b6d307fb1ab57d9e823eca44afac03d0fc525568
Bret Hayes <bret_hayes@comcast.net>
2019-03-20 08:20:00 -0400

    working on market/ capacity

--- Planets/Market.cs
+++ Planets/Market.cs
@@ -10,14 +10,14 @@ namespace Planets
 {
     class Market
     {
+        Metal metalPrice = new Metal();
+        Fabric fabricPrice = new Fabric();
+        Gemstones gemstonePrice = new Gemstones();
+        Supplies supplyPrice = new Supplies();
+        Ship vesselHold = new Ship();
+
         public void MarketDisplay()
         {
-            Metal metalPrice = new Metal();
-            Fabric fabricPrice = new Fabric();
-            Gemstones gemstonePrice = new Gemstones();
-            Supplies supplyPrice = new Supplies();
-            Ship vesselHold = new Ship();
-
             Console.WriteLine("Which resource would you like to view? \n1 - Metals\t 2 - Fabrics\n3 - Gemstone\t 4 - Supplies");
             string choice = Console.ReadLine();
             switch (choice)
@@ -74,7 +74,32 @@ namespace Planets
         }
         public void MetalMarketSell()
         {
-            
+            Console.WriteLine("Which type of Metal: \n1 - Platinium\n2 - Palladium\n3 - Titanium");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine($"{vesselHold.platCrate(0)} platinum crates\n How many do you wish to sell");
+                    int answer = int.Parse(Console.ReadLine());
+                    answer *= -1;
+                    vesselHold.platCrate(answer);
+                    break;
+                case "2":
+                    Console.WriteLine($"{vesselHold.palladCrate(0)} palladium crates\n How many do you wish to sell");
+                    answer = int.Parse(Console.ReadLine());
+                    answer *= -1;
+                    vesselHold.palladCrate(answer);
+                    break;
+                case "3":
+                    Console.WriteLine($"{vesselHold.titanCrate(0)} titanium crates\n How many do you wish to sell");
+                    answer = int.Parse(Console.ReadLine());
+                    answer *= -1;
+                    vesselHold.titanCrate(answer);
+                    break;
+                default:
+                    
+                    break;
+            }
         }
         public void MetalMarketBuy()
         {
--- Vessel/Ship.cs
+++ Vessel/Ship.cs
@@ -18,8 +18,8 @@ namespace Vessel
         }
         public int Capacity(int input)  // tracking number of crates, and limiting amount allowed
         {
-            int hold = 0;
-            hold += input;
+            int hold = metalCapacity(1) + fabricCapacity(1) + gemCapacity(1) + supplyCapacity(1);
+            hold -= input;
             if (hold > 20)
             {
                 Console.WriteLine("Overburdened, ditching crates till at max capacity of 20");
@@ -30,6 +30,164 @@ namespace Vessel
             }
             crateTrack.Crates(hold);
             return hold;
+        }
+
+        public int metalCapacity(int input) //keep track of different materials crates
+        {
+            Console.WriteLine("Which type of Metal: \n1 - Platinium\n2 - Palladium\n3 - Titanium");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine($"{platCrate(0)} crates of platinium");
+                    break;
+                case "2":
+                    Console.WriteLine($"{palladCrate(0)} crates of palladium");
+                    break;
+                case "3":
+                    Console.WriteLine($"{titanCrate(0)} crates of titanium");
+                    break;
+                default:
+                    Console.WriteLine("Invaild Input");
+                    break;
+            }
+            input = platCrate(0) + palladCrate(0) + titanCrate(0);
+            return input;
+        }
+
+        public int platCrate(int input)
+        {
+            int store = 0;
+            store += input;
+            return store;
+        }
+
+        public int palladCrate(int input)
+        {
+            int store = 0;
+            store += input;
+            return store;
+        }
+
+        public int titanCrate(int input)
+        {
+            int store = 0;
+            store += input;
+            return store;
+        }
+
+        public int fabricCapacity(int input)
+        {
+            Console.WriteLine("Which type of Metal: \n1 - Platinium\n2 - Palladium\n3 - Titanium");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine($"{platCrate()} crates of platinium");
+                    break;
+                case "2":
+                    Console.WriteLine($"{palladCrate()} crates of palladium");
+                    break;
+                case "3":
+                    Console.WriteLine($"{titanCrate()} crates of titanium");
+                    break;
+                default:
+                    Console.WriteLine("Invaild Input");
+                    break;
+            }
+            input = platCrate() + palladCrate() + titanCrate();
+            return input;
+        }
+
+        public int silkCrate()
+        {
+            return 1;
+        }
+
+        public int cottonCrate()
+        {
+            return 1;
+        }
+
+        public int linenCrate()
+        {
+            return 1;
+        }
+
+        public int gemCapacity(int input)
+        {
+            Console.WriteLine("Which type of Metal: \n1 - Platinium\n2 - Palladium\n3 - Titanium");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine($"{platCrate()} crates of platinium");
+                    break;
+                case "2":
+                    Console.WriteLine($"{palladCrate()} crates of palladium");
+                    break;
+                case "3":
+                    Console.WriteLine($"{titanCrate()} crates of titanium");
+                    break;
+                default:
+                    Console.WriteLine("Invaild Input");
+                    break;
+            }
+            input = platCrate() + palladCrate() + titanCrate();
+            return input;
+        }
+
+        public int diaCrate()
+        {
+            return 1;
+        }
+        public int rubyCrate()
+        {
+            return 1;
+        }
+        public int sappCrate()
+        {
+            return 1;
+        }
+
+        public int supplyCapacity(int input)
+        {
+            Console.WriteLine("Which type of Metal: \n1 - Platinium\n2 - Palladium\n3 - Titanium");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine($"{platCrate()} crates of platinium");
+                    break;
+                case "2":
+                    Console.WriteLine($"{palladCrate()} crates of palladium");
+                    break;
+                case "3":
+                    Console.WriteLine($"{titanCrate()} crates of titanium");
+                    break;
+                default:
+                    Console.WriteLine("Invaild Input");
+                    break;
+            }
+            input = platCrate() + palladCrate() + titanCrate();
+            return input;
+        }
+
+        public int foodCrate()
+        {
+            return 1;
+        }
+        public int waterCrate()
+        {
+            return 1;
+        }
+        public int alcoCrate()
+        {
+            return 1;
+        }
+        public int mediCrate()
+        {
+            return 1;
         }
 
         public double WarpEquation(int warp)

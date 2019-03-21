ded35fe5516be4b95f5385a462b2696a552f271f
Bret Hayes <bret_hayes@comcast.net>
2019-03-21 11:14:04 -0400

    first switch statement of Run method

--- SolVoyager/GameRun.cs
+++ SolVoyager/GameRun.cs
@@ -16,11 +16,14 @@ namespace SolVoyager
         Earth planetEarth = new Earth();
         MiningColony planetColony = new MiningColony();
         AlphaCentari planetCentari = new AlphaCentari();
-        TauCeti planeCeti = new TauCeti();
+        TauCeti planetCeti = new TauCeti();
+        PlanetDistance travelTime = new PlanetDistance();
         Ship playerShip = new Ship();
 
         public void Run()
         {
+            string location = "Earth";
+            bool check = true;
             // Opening page, with quit or start game option
             // Run Character Creation
             // Into buying your first resource to trade to another system
@@ -28,6 +31,27 @@ namespace SolVoyager
             // slowly increase payloads with longer play, forced to start with 1 maybe 2 crates
             playerTrader.Gender();
             playerTrader.Name();
+            Console.WriteLine("Which planet do you wish to start on?\nEarth\tTau Ceti\nMining Colony\tAlpha Centari");
+            // begin journey on Earth
+            while (check == true)
+            {
+                switch (location)
+                {
+                    case "Earth":
+                        planetEarth.earthDisplay();
+                        break;
+                    case "Tau Ceti":
+                        planetCeti.tauDisplay();
+                        break;
+                    case "Mining Colony":
+                        planetColony.colonyDisplay();
+                        break;
+                    case "Alpha Centari":
+                        planetCentari.alphaDisplay();
+                        break;                    
+                }
+
+            }
         }
     }
 }

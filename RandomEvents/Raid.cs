c76c5cdb3dddf5cfdbd13d42136b95f38d091c6d
munozanthony <47787298+munozanthony@users.noreply.github.com>
2019-03-25 10:18:55 -0400

    random event array updated

--- RandomEvents/Raid.cs
+++ RandomEvents/Raid.cs
@@ -30,11 +30,11 @@ namespace RandomEvents
             Random rnd = new Random();
             Console.WriteLine("You are traveling");
 
-            string[] events = new string[] {"Go straight to planet", $"{repair1}", "Raided-take half goods", "Asteroid hits ship(-250 creds to repair ship)",
-            "Warp speed engine gives out(repair cost -500 creds)", "Solar flare knocks out power(-300 creds to repair ship)", "Find abandoned trade vessel(500 creds)", "Rescue ship members, proceed to destination",
-            "Eat a bad space burrito. You have a bad night. Proceed to destination.", "Find space wreckage. Nothing recovered :(", "Find space wreckage (Recover 1 crate of Palladium)",
-            "Find abondoned ship. One of your shipmates is killed searching the wreckage. You must hire another shipmate(Cost 500 creds)", "Proceed to destination", "Keep on trucking",
-            "Safe Travels", "You're on your way to greatness", "You took a wrong turn...Check the coordinates next time", "Found 1 crate of Palladium hidden in cargo area (+1 Palladium)",
+            string[] events = new string[] {"Go straight to planet", $"{repair1}", $"{goodsTaken}", $"{repair2}",
+            $"{repair3}", $"{repair4}", $"{recoverGoods}", "Rescue ship members, proceed to destination",
+            "Eat a bad space burrito. You have a bad night. Proceed to destination.", "Find space wreckage. Nothing recovered :(", $"{foundPalladium1}",
+            $"{hireMate}", "Proceed to destination", "Keep on trucking",
+            "Safe Travels", "You're on your way to greatness", "You took a wrong turn...Check the coordinates next time", $"{foundPalladium2}",
             "Can't seem to find your lucky socks...hmmm must be space gnomes", "Space twister in zero gravity = family fun night...Continue to destination"};
 
 
